ff32ba26fde6bc52e677c55d24edd620e8d7c8b6
joshyan1 <jyan00017@gmail.com>
2024-03-09 14:57:10 -0500

    toggled follow, authentiation, editing dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,23 +2,58 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Dashboard(filter) {
+function Dashboard(userProfile) {
     const [games, setGames] = useState([]);
+    const [followed, setFollow] = useState(false);
+
+    const toggleFollow = () => {
+        setFollow(current => !current);
+      };
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/get-nhl',
         {
             params:{
-                followed: "True",
-            }
+                followed: followed,
+                user_id: userProfile.userProfile.user_id,
+            },
+            headers: {
+                "Authorization": `Bearer ${userProfile.userProfile.token}`, // Bearer scheme
+                "Content-Type": "application/json"
+              }
 
         })
             .then(response => {
-                //console.log(response.data);
-                setGames(response.data);
+                if (JSON.stringify(response.data) !== '{}'){
+                    setGames(response.data);
+                }else{
+                    setGames([]);
+                }
+            
             })
             .catch(error => console.error('Error fetching data:', error));
-    }, []);
+    }, [followed]);
+
+    const updateTeam = async (teamName) => {
+        const url = 'http://localhost:8000/api/add-fav'; // Your API endpoint
+        const sessionToken = userProfile.userProfile.token; // The session token you have
+        console.log(userProfile.userProfile.token);
+
+        try {
+          const response = await axios.put(url, {
+            name: teamName, // Assuming your backend expects the team name in the body
+          }, {
+            headers: {
+              'Authorization': `Bearer ${sessionToken}`, // Include the session token in the Authorization header
+            }
+          });
+          
+          console.log('Update successful', response.data);
+        } catch (error) {
+          console.error('Error updating team', error);
+        }
+      };
+      
 
     return (
         <div>
@@ -27,6 +62,12 @@ function Dashboard(filter) {
                 //console.log(game.status)
                 return(
                     <div key={game?.id}>
+                        <div>
+                            <p>You are {followed ? 'currently' : 'not'} following.</p>
+                            <button onClick={toggleFollow}>
+                                {followed ? 'Unfollow' : 'Follow'}
+                            </button>
+                        </div>
                         <h2>{game?.name}</h2>
                         <p>Game Clock: Period {game.status['period']}: {game.status['displayClock']}</p>
                         <p>Home Team: {game?.home_team} -- Score: {game?.home_score}</p>
